05fa344d9f033cb82b889dd5c47b8848c2903e2e
chenruisilalala <33834625+chenruisilalala@users.noreply.github.com>
2020-01-21 20:40:15 +0800

    Update diy.sh
    kfermer to tty228

--- diy.sh
+++ diy.sh
@@ -7,4 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/KFERMercer/luci-app-serverchan.git  package/openwrt/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/luci-app-serverchan
