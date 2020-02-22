bfdcaf669389b602b62778f721e4b6b3f5f7b055
chenruisilalala <33834625+chenruisilalala@users.noreply.github.com>
2020-02-22 14:59:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/serverchan;
+mv package/lean/luci-theme-netgear/ package/openwrt/;
+mv package/lean/luci-theme-argon/ package/openwrt/;
