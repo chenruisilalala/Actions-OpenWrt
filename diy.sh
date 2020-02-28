092750b99701700d362e5eb469d7a53c28932df3
chenruisilalala <33834625+chenruisilalala@users.noreply.github.com>
2020-02-28 21:23:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,14 +6,13 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/serverchan
-#mv package/lean/luci-theme-netgear/ package/openwrt/netgear
-git clone git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
-mv  package/openwrt/lie/lienol/luci-theme-netgear-mc/ package/openwrt/
-git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
-mv package/openwrt/lie/lienol/luci-app-passwall package/openwrt/passwall
-rm -rf package/openwrt/lie
-echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
-git clone https://github.com/vernesong/OpenClash.git package/openwrt/openclash
-./scripts/feeds update -a ; ./scripts/feeds install -a
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate;
+git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/luci-app-serverchan ;
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie ;
+mv  package/openwrt/lie/lienol/luci-theme-netgear-mc/ package/openwrt/ ;
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie ;
+mv package/openwrt/lie/lienol/luci-app-passwall package/openwrt/passwall ;
+rm -rf package/openwrt/lie ;
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default ;
+git clone https://github.com/vernesong/OpenClash.git package/openwrt/openclash ;
+./scripts/feeds clean ; ./scripts/feeds update -a ; ./scripts/feeds install -a
