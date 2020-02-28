5a64775271010d696bad40cf53546f66bb86e6f5
chenruisilalala <33834625+chenruisilalala@users.noreply.github.com>
2020-02-29 00:57:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,18 +6,15 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate ;
-./scripts/feeds update -a ; ./scripts/feeds install -a ;
-echo 'feeds1' ;
-git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/luci-app-serverchan ;
-git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie ;
-mv  package/openwrt/lie/lienol/luci-theme-netgear-mc/ package/openwrt/ ;
-git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie ;
-mv package/openwrt/lie/lienol/luci-app-passwall package/openwrt/passwall ;
-rm -rf package/openwrt/lie ;
-echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default ;
-git clone https://github.com/vernesong/OpenClash.git package/openwrt/openclash ;
-./scripts/feeds clean ; ./scripts/feeds update -a ; ./scripts/feeds install -a ;
-echo 'feeds2' ;
-./scripts/feeds update -a ; ./scripts/feeds install -a ;
-echo 'feeds3' ;
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+./scripts/feeds update -a ; ./scripts/feeds install -a
+git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/luci-app-serverchan
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
+mv  package/openwrt/lie/lienol/luci-theme-netgear-mc/ package/openwrt/
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
+mv package/openwrt/lie/lienol/luci-app-passwall package/openwrt/passwall
+rm -rf package/openwrt/lie
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+git clone https://github.com/vernesong/OpenClash.git package/openwrt/openclash
+./scripts/feeds clean ; ./scripts/feeds update -a ; ./scripts/feeds install -a
+./scripts/feeds update -a ; ./scripts/feeds install -a
