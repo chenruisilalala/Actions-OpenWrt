91395953f0f1f121282de614e85373efe6556c50
chenruisilalala <33834625+chenruisilalala@users.noreply.github.com>
2020-02-28 20:30:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/serverchan
 #mv package/lean/luci-theme-netgear/ package/openwrt/netgear
+git clone git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
 mv  package/openwrt/lie/lienol/luci-theme-netgear-mc/ package/openwrt/
 git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
 mv package/openwrt/lie/lienol/luci-app-passwall package/openwrt/passwall
