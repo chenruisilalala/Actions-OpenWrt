27b76ccea54c2c5703792f19304d2976c95223e9
chenruisilalala <33834625+chenruisilalala@users.noreply.github.com>
2020-02-28 21:36:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate;
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate ;
 ./scripts/feeds update -a ; ./scripts/feeds install -a ;
 git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/luci-app-serverchan ;
 git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie ;
@@ -17,3 +17,4 @@ rm -rf package/openwrt/lie ;
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default ;
 git clone https://github.com/vernesong/OpenClash.git package/openwrt/openclash ;
 ./scripts/feeds clean ; ./scripts/feeds update -a ; ./scripts/feeds install -a ;
+echo 'customfiledone' ;
