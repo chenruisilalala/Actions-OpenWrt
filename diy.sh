444d9bcc0ed3bced3850138b0cd02e9605af79a7
ciring <33834625+ciring@users.noreply.github.com>
2021-06-24 23:04:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 ./scripts/feeds update -a ; ./scripts/feeds install -a
 git clone https://github.com/tty228/luci-app-serverchan.git  package/openwrt/luci-app-serverchan
 git clone https://github.com/fw876/helloworld.git package/openwrt/ssr
-mv package/openwrt/ssr/luci-app-ssr-plus/  package/openwrt/
-rm -rf package/openwrt/ssr/
+#mv package/openwrt/ssr/luci-app-ssr-plus/  package/openwrt/
+#rm -rf package/openwrt/ssr/
 #git clone https://github.com/Lienol/openwrt-package.git package/openwrt/lie
 #mv package/openwrt/lie/lienol/luci-app-pppoe-relay package/openwrt/luci-app-pppoe-relay
 #mv package/openwrt/lie/lienol/luci-app-passwall package/openwrt/luci-app-passwall
